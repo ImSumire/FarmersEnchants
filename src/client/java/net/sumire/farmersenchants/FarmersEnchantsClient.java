92eada0c0ca414a3c602c3a173e3194227fef609
ImSumire <galaad.fenneteau@gmail.com>
2024-05-21 23:17:55 +0200

    Initial commit

---
+++ src/client/java/net/sumire/farmersenchants/FarmersEnchantsClient.java
@@ -0,0 +1,10 @@
+package net.sumire.farmersenchants;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FarmersEnchantsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
